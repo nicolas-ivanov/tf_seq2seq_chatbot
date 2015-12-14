0bec73eac9159f3f46b09ef60c5f68a784b2fef3
nicolas-ivanov <nickolas.ivanov@gmail.com>
2015-12-14 15:44:24 +0300

    add module directory to python root path

--- bin/test.py
+++ bin/test.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import tensorflow as tf
 
 from tf_seq2seq_chatbot.lib.predict import predict
